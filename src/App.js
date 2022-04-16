75c5a13cb47d50d8ca06837824638b24112e3d01
unknown <barketifarah4@gmail.com>
2022-04-16 16:06:38 +0200

    mon-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Todolist from './components/Todolist';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+ <h1>FORMS</h1>
+     <Todolist/>
+    </div>
+  );
+}
+export default App;
